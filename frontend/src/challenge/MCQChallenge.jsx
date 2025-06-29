3f0bbe0504a629fbdb72367772cb6f384b5a5be1
Arthur Faria <arthur.agf@gmail.com>
2025-06-29 15:56:29 -0300

    Implements the MCQChallenge component

--- frontend/src/challenge/MCQChallenge.jsx
+++ frontend/src/challenge/MCQChallenge.jsx
@@ -1,5 +1,44 @@
 import "react"
+import {useState} from "react"
 
-export default function MCQChallenge() {
-    return <></>
+export default function MCQChallenge({challenge, showExplanation = false}) {
+    const [selectedOption, setSelectedOption] = useState(null)
+    const [shouldShowExplanation, setShouldShowExplanation] = useState(showExplanation)
+    
+    const options = typeof challenge.options === "string" ? 
+        JSON.parse(challenge.options) : 
+        challenge.options
+        
+    const handleOptionSelect = (index) => {
+        if (selectedOption) !== null) return;
+        setSelectedOption(index)
+        setShouldShowExplanation(true)
+    }
+
+    const getOptionClass = (index) => {
+        if (selectedOption === null) return "option";}
+        if (index === challenge.correct_answer_id) return "option correct";
+        if (index === selectedOption && index !== challenge.correct_answer_id)  return "option incorrect";
+        return "option";
+    }
+    
+    return <div className="challenge-display">
+        <p><strong>Difficulty:</strong> {challenge.difficulty}</p>
+        <p className="challenge-title">{challenge.title}</p>
+        <div className="options">
+            {options.map((option, index) => (
+                <div 
+                    key={index} 
+                    className={getOptionClass(index)} 
+                    onClick={() => handleOptionSelect(index)}>
+                    {option}
+                </div>
+            ))}
+        </div>
+        {shouldShowExplanation && selectedOption !== null && (
+            <div className="explanation">}
+                <p><strong>Explanation:</strong> {challenge.explanation}</p>
+            </div>
+        )}
+            
 }

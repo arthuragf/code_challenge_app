843845252a0b26cd1a2d0a11d99419661c0e231b
Arthur Faria <arthur.agf@gmail.com>
2025-07-03 22:43:12 -0300

    Implements the fetchHistory function

--- frontend/src/history/HistoryPanel.jsx
+++ frontend/src/history/HistoryPanel.jsx
@@ -1,18 +1,30 @@
 import "react"
 import {useState, useEffect} from "react"
 import MCQChallenge from "../challenge/MCQChallenge.jsx"
+import {UseApi} from "../utils/api.js"
 
 export default function HistoryPanel() {
     const [history, setHistory] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [error, setError] = useState(null)
+    const {makeRequest} = UseApi()
     
     useEffect(() => {
         fetchHistory()
     }, [])
 
     const fetchHistory = async () => {
-        setIsLoading(false)
+        setIsLoading(true)
+        setError(null)
+        try {
+            const data = await makeRequest("my-history")
+            setHistory(data?.challenges)
+        } catch (err) {
+            setError("Failed to load history" + err.message || "An error occurred while fetching history.")
+        } finally {
+            setIsLoading(false)
+        }
+        
     }
 
     if (isLoading) {
